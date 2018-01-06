562d4114f074a3e4052ab286273fc177e28b5294
svladkov <s.v.vladkov@gmail.com>
2018-01-07 01:45:35 +0200

    Added database connection string to example configuration file

--- exampleConfig.js
+++ exampleConfig.js
@@ -1,6 +1,7 @@
 const config = {}
 
-config.port = 'some port';
+config.port = 3000;
+config.connectionString = 'mongodb://localhost/weather-db';
 config.weatherApiKey = 'some api key';
 
 module.exports = config;
